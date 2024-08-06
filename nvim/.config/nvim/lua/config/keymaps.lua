3b514807d7f9697833d89b4e68fbb92011d1c0fa
byteZorvin <imehul24@gmail.com>
2024-08-06 16:55:34 +0530

    remove unnessary keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,13 +7,3 @@ vim.keymap.set("n", "<C-f>", ":!tmux neww tmux-sessionizer<CR>", { noremap = tru
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
--- vim.keymap.set("n", "<C-j>", ":echo 'Alt-J pressed'<CR>", { desc = "Test Alt-J" })
-vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-
-vim.keymap.set("n", "<D-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-vim.keymap.set("n", "<D-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-vim.keymap.set("i", "<D-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-vim.keymap.set("i", "<D-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
--- vim.keymap.set("i", "<") fc
