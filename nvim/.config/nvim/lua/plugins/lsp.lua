2e5a03ece01523b6a58bc95dfa62cff9c299a247
byteZorvin <imehul24@gmail.com>
2024-09-25 11:27:15 +0530

    fix to use mason-lspconfig

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,16 +1,39 @@
--- if true then
---   return {
---     "williamboman/mason-lspconfig.nvim",
---     dependencies = {
---       "williamboman/mason.nvim",
---       "neovim/nvim-lspconfig",
---     },
---
+return {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+  },
+
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+
+    require("lspconfig").rust_analyzer.setup({
+      settings = {
+        ["rust-analyzer"] = {
+          checkOnSave = {
+            command = "clippy",
+            extraArgs = { "--no-deps" }, -- Optional: add this if you don't want to check dependencies
+          },
+          cargo = {
+            features = { "testing" }, -- Replace "feat1" with your specific feature
+            allFeatures = false, -- Disable all features by default
+          },
+          -- You can add more rust-analyzer settings here
+        },
+      },
+    })
+  end,
+}
+
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
 --     config = function()
---       require("mason").setup()
---       require("mason-lspconfig").setup()
+--       local lspconfig = require("lspconfig")
 --
---       require("lspconfig").rust_analyzer.setup({
+--       lspconfig.rust_analyzer.setup({
 --         settings = {
 --           ["rust-analyzer"] = {
 --             checkOnSave = {
@@ -26,30 +49,6 @@
 --         },
 --       })
 --     end,
---   }
--- end
-
-return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-
-      lspconfig.rust_analyzer.setup({
-        settings = {
-          ["rust-analyzer"] = {
-            check = {
-              command = "clippy",
-            },
-            cargo = {
-              features = { "testing" }, -- Replace "feat1" with your specific feature
-              allFeatures = false, -- Disable all features by default
-            },
-            -- You can add more rust-analyzer settings here
-          },
-        },
-      })
-    end,
-  },
-  -- -- Other plugins...
-}
+--   },
+--   -- -- Other plugins...
+-- }
